86be3fe95dbd2e074a48e088c0c9cdcc57ffc967
Cocor <mac2222.ai@gmail.com>
2021-11-19 10:17:27 +0100

    Variant 2

--- script_01.js
+++ script_01.js
@@ -1,17 +1,28 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration
-let ageJohn, ageMark;
-let isJohnOlder;
+// let ageJohn, ageMark;
+// let isJohnOlder;
+
+// Deklaration + Assigment
+const ageJohn = 30;
+const ageMark = 35;
 
 // Assigment (Wertzuweisung)
+// ageJohn = 30;
+// ageMark = 25;
 
-ageJohn = 30;
-ageMark = 25;
+// Deklaration
+let isJohnOlder, isJohnEqual;
 
-
+//Test Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
-console.log(typeof isJohnOlder)
+isJohnEqual = (ageJohn == ageMark);
 
-console.log(ageJohn);
+// console.log(typeof isJohnOlder)
+
+// Ausgabe
+console.log("ageJohn" + ageJohn);
+console.log("ageMark" + ageMark);
 console.log("isJohnOlder:" + isJohnOlder);
+console.log("isJohnEqual:" + isJohnEqual);
