5ed2004b64259d9cab7482da2fe5f3ec09d1bb96
Cocor <mac2222.ai@gmail.com>
2021-11-19 10:43:21 +0100

    Variant 2

--- script_01.js
+++ script_01.js
@@ -1,28 +1,41 @@
-// Entscheidungsstrukturen | control flow
+// // Entscheidungsstrukturen | control flow
 
-// Deklaration //**** Variant 1 **/
-// let ageJohn, ageMark;
-// let isJohnOlder;
+// // Deklaration //**** Variant 1 **/
+// // let ageJohn, ageMark;
+// // let isJohnOlder;
 
-// Deklaration + Assigment
+// // Deklaration + Assigment
 const ageJohn = 30;
-const ageMark = 30;
+const ageMark = 29;
 
-// Assigment (Wertzuweisung) //****  Variant 1 **/
-// ageJohn = 30;
-// ageMark = 25;
+// // Assigment (Wertzuweisung) //****  Variant 1 **/
+// // ageJohn = 30;
+// // ageMark = 25;
 
-// Deklaration
+// // Deklaration
 let isJohnOlder, isJohnEqual;
 
-//Test Logische Aussage
+// //Test Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
-// console.log(typeof isJohnOlder) //****  Variant1 **/
+// // console.log(typeof isJohnOlder) //****  Variant1 **/
 
-// Ausgabe
-console.log("ageJohn" + ageJohn);
-console.log("ageMark" + ageMark);
-console.log("isJohnOlder:" + isJohnOlder);
-console.log("isJohnEqual:" + isJohnEqual);
+// // Ausgabe
+// console.log("ageJohn" + ageJohn);
+// console.log("ageMark" + ageMark);
+// console.log("isJohnOlder:" + isJohnOlder);
+// console.log("isJohnEqual:" + isJohnEqual);
+
+
+/**************** IF ************* */
+// TINA ----> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+
+// if (true) 
+// if (false) 
+if(isJohnOlder)
+{
+    console.log("John ist älter");
+}
