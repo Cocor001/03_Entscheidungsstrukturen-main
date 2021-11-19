db88db4a184f804106390bb0583773be1cf652f0
Cocor <mac2222.ai@gmail.com>
2021-11-19 09:32:35 +0100

    Age Answers

---
+++ script_01.js
@@ -0,0 +1,17 @@
+// Entscheidungsstrukturen | control flow
+
+// Deklaration
+let ageJohn, ageMark;
+let isJohnOlder;
+
+// Assigment (Wertzuweisung)
+
+ageJohn = 30;
+ageMark = 25;
+
+
+isJohnOlder = (ageJohn > ageMark);
+console.log(typeof isJohnOlder)
+
+console.log(ageJohn);
+console.log("isJohnOlder:" + isJohnOlder);
